c40e0a6a3e2a55dee24660de2f1a08ee2fb333f5
natnaelian <natnaelian@gmail.com>
2025-07-07 13:38:48 +0300

    Add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("Subtraction of ",number1, "and ",number2, "is =",number1-number2)
+print("Addition of ",number1, "and ",number2, "is =",number1+number2)
+print("Product of ",number1, "and ",number2 ,"is =",number1*number2)
