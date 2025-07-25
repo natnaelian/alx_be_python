3df3b4f7a42603b6f3bb226cd806698d21f87a22
natnaelian <natnaelian@gmail.com>
2025-07-25 22:16:55 +0300

    fas

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,3 +1,19 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cannot divide by zero."
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation."
+from arithmetic_operations import perform_operation
+
 def main():
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
