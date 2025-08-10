ff89ed7250e19de2723e917002348a811efcf468
natnaelian <natnaelian@gmail.com>
2025-08-10 11:24:03 +0300

    fskdjgs

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,16 +1,11 @@
-# class_static_methods_demo.py
-
 class Calculator:
-    # Class attribute
     calculation_type = "Arithmetic Operations"
 
     @staticmethod
     def add(a, b):
-        """Return the sum of two numbers."""
         return a + b
 
     @classmethod
     def multiply(cls, a, b):
-        """Return the product of two numbers after printing the calculation type."""
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
